4fb58021a1f4a7b3252090161f599c1fd1875d8d
Kona01 <matthewjacobs@hotmail.ca>
2017-10-31 13:23:10 -0700

    Added Trump Gem

---
+++ src/main/java/mer/mod/items/ItemTrumpGem.java
@@ -0,0 +1,12 @@
+package mer.mod.items;
+
+import mer.mod.Reference;
+import net.minecraft.item.Item;
+
+public class ItemTrumpGem extends Item {
+
+    public ItemTrumpGem() {
+        setUnlocalizedName(Reference.ModItems.TRUMPGEM.getUnlocalizedName());
+        setRegistryName(Reference.ModItems.TRUMPGEM.getRegistryName());
+    }
+}
