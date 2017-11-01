2102dbd5b42785496cfed81187ad8871930d5180
pixelkips <pixelkips@outlook.com>
2017-10-31 18:58:08 -0700

    Everything works now!

--- src/main/java/mer/mod/Reference.java
+++ src/main/java/mer/mod/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MOD_ID = "mer";
     public static final String NAME = "mer";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.0";
     public static final String ACCEPTED_VERSIONS = "[1.11.2]";
 
     public static final String CLIENT_PROXY_CLASS = "mer.mod.proxy.ClientProxy";
