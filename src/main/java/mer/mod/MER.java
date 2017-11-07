479a2e19a7f62bd7541f17ad13f2607884121442
Elliott S <pixelkips@outlook.com>
2017-11-07 15:20:37 -0800

    Everything works (I hope)

--- src/main/java/mer/mod/MER.java
+++ src/main/java/mer/mod/MER.java
@@ -31,7 +31,6 @@ public class MER {
         ModBlocks.register();
         ModItems.init();
         ModItems.register();
-        ModCrafting.register();
     }
 
     @Mod.EventHandler
--- src/main/java/mer/mod/init/ModCrafting.java
+++ src/main/java/mer/mod/init/ModCrafting.java
@@ -8,7 +8,8 @@ public class ModCrafting {
 
     public static void register(){
         GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumpsword), " T ", " T ", " S ", 'T', ModItems.trumpgem, 'S', Items.STICK);
+        GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumpaxe), "TT ", "TS ", " S ", 'T', ModItems.trumpgem, 'S', Items.STICK);
+        GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumpboots), "", "T T", "T T", 'T', ModItems.trumpgem);
+        GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumpchestplate),"T T", "TTT", "TTT", 'T', ModItems.trumpgem);
     }
-
-
 }
