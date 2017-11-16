36f199fd658e8a88222357ecb7e05598ef58a57f
Elliott S <pixelkips@outlook.com>
2017-11-16 08:46:07 -0800

    Trump model correction

--- src/main/java/mer/mod/entities/EntityZombieWolf.java
+++ src/main/java/mer/mod/entities/EntityZombieWolf.java
@@ -3,45 +3,39 @@ package mer.mod.entities;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.*;
 import net.minecraft.entity.monster.EntityIronGolem;
-import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.passive.EntityWolf;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 
+public class EntityZombieWolf extends EntityWolf {
 
-import javax.annotation.Nullable;
+    public static final ResourceLocation LOOT = new ResourceLocation("mer:entities/zombiewolf");
 
-public class EntityTrump extends EntityMob {
-
-    public static final ResourceLocation LOOT = new ResourceLocation("mer:entities/trump");
-
-    public EntityTrump(World worldin){
+    public EntityZombieWolf(World worldin){
         super(worldin);
-        setSize(0.6F, 1.95F);
     }
 
     @Override
-    protected void entityInit() {
+    protected void entityInit(){
         super.entityInit();
     }
-
 
     @Override
     protected void applyEntityAttributes() {
         super.applyEntityAttributes();
         // Here we set various attributes for our mob. Like maximum health, armor, speed, ...
         this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(35.0D);
-        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(250.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(20.0D);
         this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(10.D);
-        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(55.0D);
-        this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(35.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(1.5D);
     }
 
     @Override
     protected void initEntityAI() {
         this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(2, new EntityAITrumpAttack(this, 3.0D, false));
+        this.tasks.addTask(2, new EntityAIZombieWolfAttack(this, 3.0D, false));
         this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
         this.tasks.addTask(7, new EntityAIWander(this, 5.0D));
         this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
@@ -53,27 +47,6 @@ public class EntityTrump extends EntityMob {
         this.tasks.addTask(6, new EntityAIMoveThroughVillage(this, 1.0D, false));
         this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
         this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this, EntityVillager.class, true));
-        this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this, EntityIronGolem.class, true));
-    }
-
-
-    @Override
-    @Nullable
-    protected ResourceLocation getLootTable() {
-        return LOOT;
-    }
-
-    @Override
-    protected boolean isValidLightLevel() {
-        return true;
-    }
-
-    @Override
-    public int getMaxSpawnedInChunk() {
-        return 5;
-    }
-
-
-
+}
 
 }
