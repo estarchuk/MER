1134ca3e3de6117df6269c24ecc6434c1dc0af05
Kona01 <matthewjacobs@hotmail.ca>
2017-11-25 21:13:13 -0800

    Added all(?) crafting recipes, and most missing textures

--- src/main/java/mer/mod/init/ModCrafting.java
+++ src/main/java/mer/mod/init/ModCrafting.java
@@ -13,19 +13,30 @@ public class ModCrafting {
     of the crafting table. The first set is the top row, the second the middle row, and the third the bottom row.
      */
 
-        public static void register(){GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumpsword), " T ", " T ", " S ", 'T', ModBlocks.trumpiumblock, 'S', Items.STICK);
+        public static void register(){
+            GameRegistry.addShapedRecipe(new ItemStack(ModBlocks.trumpiumblock), "TTT", "TTT", "TTT", 'T', ModItems.trumpgem);
+            GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumpsword), " T ", " T ", " S ", 'T', ModBlocks.trumpiumblock, 'S', Items.STICK);
             GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumpaxe), "TT ", "TS ", " S ", 'T', ModBlocks.trumpiumblock, 'S', Items.STICK);
-            GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumpboots), "   ", "T T", "T T", 'T', ModBlocks.trumpiumblock);
+            GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumppickaxe), "TTT", " S ", " S ", 'T', ModBlocks.trumpiumblock, 'S', Items.STICK);
+            GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumpspade), " T ", " S ", " S ", 'T', ModBlocks.trumpiumblock, 'S', Items.STICK);
+            GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumphoe), "TT ", " S ", " S ", 'T', ModBlocks.trumpiumblock, 'S', Items.STICK);
+            GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumphelmet), "TTT", "T T", "   ", 'T', ModBlocks.trumpiumblock);
             GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumpchestplate),"T T", "TTT", "TTT", 'T', ModBlocks.trumpiumblock);
             GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumpleggings), "TTT", "T T", "T T", 'T', ModBlocks.trumpiumblock);
-            GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumphelmet), "TTT", "T T", "   ", 'T', ModBlocks.trumpiumblock);
-            GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumppickaxe), "TTT", " S ", " S ", 'T', ModBlocks.trumpiumblock, 'S', Items.STICK);
-            GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumphoe), "TT ", " S ", " S ", 'T', ModBlocks.trumpiumblock, 'S', Items.STICK);
-            GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumpspade), " T ", " S ", " S ", 'T', ModBlocks.trumpiumblock, 'S', Items.STICK);
+            GameRegistry.addShapedRecipe(new ItemStack(ModItems.trumpboots), "   ", "T T", "T T", 'T', ModBlocks.trumpiumblock);
+            GameRegistry.addShapelessRecipe(new ItemStack(ModItems.steelpowder), Items.IRON_INGOT, Items.COAL);
+            GameRegistry.addShapedRecipe(new ItemStack(ModItems.steelsword), " T ", " T ", " S ", 'T', ModItems.steelingot, 'S', Items.STICK);
+            GameRegistry.addShapedRecipe(new ItemStack(ModItems.steelaxe), "TT ", "TS ", " S ", 'T', ModItems.steelingot, 'S', Items.STICK);
+            GameRegistry.addShapedRecipe(new ItemStack(ModItems.steelpickaxe), "TTT", " S ", " S ", 'T', ModItems.steelingot, 'S', Items.STICK);
+            GameRegistry.addShapedRecipe(new ItemStack(ModItems.steelspade), " T ", " S ", " S ", 'T', ModItems.steelingot, 'S', Items.STICK);
+            GameRegistry.addShapedRecipe(new ItemStack(ModItems.steelhoe), "TT ", " S ", " S ", 'T', ModItems.steelingot, 'S', Items.STICK);
+            GameRegistry.addShapedRecipe(new ItemStack(ModItems.steelhelmet), "TTT", "T T", "   ", 'T', ModItems.steelingot);
+            GameRegistry.addShapedRecipe(new ItemStack(ModItems.steelchestplate),"T T", "TTT", "TTT", 'T', ModItems.steelingot);
+            GameRegistry.addShapedRecipe(new ItemStack(ModItems.steelleggings), "TTT", "T T", "T T", 'T', ModItems.steelingot);
+            GameRegistry.addShapedRecipe(new ItemStack(ModItems.steelboots), "   ", "T T", "T T", 'T', ModItems.steelingot);
             GameRegistry.addShapelessRecipe(new ItemStack(ModItems.dough), Items.WHEAT, ModItems.salt, Items.WATER_BUCKET);
             GameRegistry.addShapelessRecipe(new ItemStack(ModItems.butter), Items.MILK_BUCKET, ModItems.salt);
             GameRegistry.addShapelessRecipe(new ItemStack(ModItems.butteredtoast), ModItems.toast, ModItems.butter);
-            GameRegistry.addShapelessRecipe(new ItemStack(ModItems.steelpowder), Items.IRON_INGOT, Items.COAL);
         }
 
 }
